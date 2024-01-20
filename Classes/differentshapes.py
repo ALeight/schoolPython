60142068c3998cdd6b925e14050f0c03ce8ce1cd
ALeight <kristoffersenbendik@gmail.com>
2024-01-20 10:27:19 +0100

    Finalized objects

--- Classes/differentshapes.py
+++ Classes/differentshapes.py
@@ -1,45 +1,26 @@
-import random
-
 import pyglet as pg
 from pyglet import shapes
 import random
+import math
+from math import pi
 
 window = pg.window.Window()
 batch = pg.graphics.Batch()
 
 
-def updatepos(dt):
-    global circle, velx, vely
-    circle.x += dt * velx
-    circle.y += dt * vely
-
-    if circle.x > window.width - circle.radius:
-        circle.x = window.width - circle
-        velx *= -1.0    # turns circle around
-    elif circle.x < circle.radius:
-        circle.x = circle.radius
-
-    if circle.y > window.height - circle.radius:
-        circle.y = window.height - circle
-        vely *= -1.0
-    elif circle.y < circle.radius:
-        circle.y = circle.radius
-
-def diffposition(x, y):
-    pos_x = random.randint(window.width - 200, window.height - 100)
-    pos_y = random.randint(window.width - 200, window.height - 100)
-    return x, y
-
-
-rectangle = shapes.Rectangle(100, 250, 100, 100, color=(255, 255, 255), batch=batch)
-ellipse = shapes.Ellipse(180, 360, 100, 85, color=(255, 255, 255), batch=batch)
-star = shapes.Star(450, 200, 200, 25, 8, color=(255, 255, 255), batch=batch)
-line = shapes.Line(700, 200, 200, 100, 100, color=(255, 255, 255), batch=batch)
+rectangle = shapes.Rectangle(650, 40, 100, 100, color=(120, 210, 150), batch=batch)
+ellipse = shapes.Ellipse(180, 300, 100, 85, color=(180, 255, 255), batch=batch)
+star = shapes.Star(700, 300, 100, 20, 6, color=(10, 255, 255), batch=batch)
+line = shapes.Line(350, 50, 300, 200, 5, color=(76, 160, 25), batch=batch)
+square = shapes.Rectangle(50, 75, 100, 100, color=(210, 210, 55), batch=batch)
+triangle = shapes.Triangle(500, 25, 600, 100, 50, 25, color=(150, 210, 150), batch=batch)
+arc = shapes.Arc(890, 75, 75, angle=3*pi/2, color=(150, 210, 150), batch=batch)
 
 
 @window.event()
 def on_draw():
     window.clear()
     batch.draw()
+
 
 pg.app.run()
